bf242e0e0dfbddb8f99be93b454c28be32e8d086
Scott Walter <scott.walter@nice.com>
2021-03-17 08:16:28 -0500

    added pipe

---
+++ pipe.js
@@ -0,0 +1,20 @@
+pipe = (...functions) => (value) => {
+    return functions.reduce((currentValue, currentFunction) => {
+      return currentFunction(currentValue);
+    }, value);
+  };
+
+  //
+  getName = (person) => person.name;
+  uppercase = (string) => string.toUpperCase();
+  get6Characters = (string) => string.substring(0, 6);
+  reverse = (string) => string.split('').reverse().join('');
+
+  pipe(
+    getName,
+    uppercase,
+    get6Characters,
+    reverse
+  )({ name: 'Buckethead' });
+  // 'TEKCUB'
+
