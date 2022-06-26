86295aec3cd665cf3d330405f0ef07e663b6becf
Scott Walter <scott.walter@nice.com>
2022-06-26 12:49:32 -0500

    updates

--- src/object-utils.js
+++ src/object-utils.js
@@ -58,3 +58,9 @@ export const singletonify = (className) => {
     }
   });
 };
+
+/*
+pick({ a: 1, b: '2', c: 3 }, ['a', 'c']); // { 'a': 1, 'c': 3 }
+*/
+export const pick = (obj, arr) =>
+  arr.reduce((acc, curr) => (curr in obj && (acc[curr] = obj[curr]), acc), {});
